96a8917c48985b1967e66200df9d3f73c24e04bc
bytecode <bytecode0101@yahoo.com>
2015-10-22 20:32:59 +0300

    Fixed errs

---
+++ UnitySample/UWar/IUnit.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWar
+{
+    public interface IUnit
+    {
+        IAttackStrategy AttacStrategy { get; set; }
+        void Attack();
+    }
+
+    public class Unit : IUnit
+    {
+        IAttackStrategy attacStrategy;
+
+       // [Dependency]
+        public IAttackStrategy AttacStrategy
+        {
+            get
+            {
+                return attacStrategy;
+            }
+
+            set
+            {
+                attacStrategy = value;
+            }
+        }
+
+        public void Attack()
+        {
+            attacStrategy.Attack();
+        }
+    }
+}
